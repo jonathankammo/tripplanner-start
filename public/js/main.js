52a93e43d464399a1dbeaafd2f6d80061fc9feb7
Jonathan Kammo <jonathankammo@gmail.com>
2017-08-15 15:44:15 -0500

    Can add to itinerary

--- public/js/main.js
+++ public/js/main.js
@@ -56,8 +56,49 @@ $(function initializeMap (){
     marker.setMap(currentMap);
   }
 
-    drawMarker('hotel', [41.8884073, -87.6293817]);
-    drawMarker('restaurant', [41.9134555, -87.6503527]);
-    drawMarker('activity', [41.8675766, -87.6162267])
-
+    // drawMarker('hotel', [41.8884073, -87.6293817]);
+    // drawMarker('restaurant', [41.9134555, -87.6503527]);
+    // drawMarker('activity', [41.8675766, -87.6162267])
 });
+
+function GenerateItinerary () {
+  this.hotels = [];
+  this.restaurants = [];
+  this.activities= [];
+}
+
+var day1 = new GenerateItinerary();
+
+for (var i = 0; i < hotels.length; i++) {
+    let hotelChoice = hotels[i].name;
+    $("#hotel-choices").append(`<option value="${i}">${hotelChoice}</option>`);
+}
+
+for (var i = 0; i < restaurants.length; i++) {
+    let restaurantChoice = restaurants[i].name;
+    $("#restaurant-choices").append(`<option value="${i}">${restaurantChoice}</option>`);
+}
+
+for (var i = 0; i < activities.length; i++) {
+    let activityChoice = activities[i].name;
+    $("#activity-choices").append(`<option value="${i}">${activityChoice}</option>`);
+}
+
+$("#options-panel").on('click', 'button', function() {
+  let index = $(this).prev().val();
+  if($(this).prev().data("type") === 'hotel') {
+    day1.hotels.push(hotels[index]);
+    $("#hotel-itinerary .itinerary-item").append("<span class=\"title\">" + hotels[index].name + "</span>");
+    $("#hotel-itinerary .itinerary-item").append("<button class=\"btn btn-xs btn-danger remove btn-circle\">x</button>");
+  } else if($(this).prev().data("type") === 'restaurant') {
+    day1.restaurants.push(restaurants[index]);
+    $("#restaurant-itinerary .itinerary-item").append("<span class=\"title\">" + restaurants[index].name + "</span>");
+    $("#restaurant-itinerary .itinerary-item").append("<button class=\"btn btn-xs btn-danger remove btn-circle\">x</button>");
+  } else if($(this).prev().data("type") === 'activity') {
+    day1.activities.push(activities[index]);
+    $("#activity-itinerary .itinerary-item").append("<span class=\"title\">" + activities[index].name + "</span>");
+    $("#activity-itinerary .itinerary-item").append("<button class=\"btn btn-xs btn-danger remove btn-circle\">x</button>");
+  }
+  console.log(day1);
+});
+
