41eef7208303071e653f61da0185fb89800256a6
Jonathan Kammo <jonathankammo@gmail.com>
2017-08-16 11:03:57 -0500

    Almost complete with switch days, final submission before classtime finished

--- public/js/main.js
+++ public/js/main.js
@@ -74,47 +74,80 @@ var currentDay = 0;
 
 for (var i = 0; i < hotels.length; i++) {
     let hotelChoice = hotels[i].name;
-    $("#hotel-choices").append(`<option value="${i}">${hotelChoice}</option>`);
+    let hotelId = hotels[i].id;
+    $("#hotel-choices").append(`<option value="${hotelId}">${hotelChoice}</option>`);
 }
 
 for (var i = 0; i < restaurants.length; i++) {
     let restaurantChoice = restaurants[i].name;
-    $("#restaurant-choices").append(`<option value="${i}">${restaurantChoice}</option>`);
+    let restaurantId = restaurants[i].id;
+    $("#restaurant-choices").append(`<option value="${restaurantId}">${restaurantChoice}</option>`);
 }
 
 for (var i = 0; i < activities.length; i++) {
     let activityChoice = activities[i].name;
-    $("#activity-choices").append(`<option value="${i}">${activityChoice}</option>`);
+    let activityId = activities[i].id;
+    $("#activity-choices").append(`<option value="${activityId}">${activityChoice}</option>`);
+}
+
+function addHotelToItinerary(index, hotel) {
+  let itineraryDiv = $("#hotel-itinerary .itinerary-item");
+  itineraryDiv.append("<span class=\"title\" data-id=" + index + " data-type=\"hotel\">" + hotel.name + "</span>");
+  itineraryDiv.append("<button class=\"btn btn-xs btn-danger remove btn-circle\">x</button>");
+  return globalDrawMarker('hotel', hotel.place.location);
+}
+
+function addRestaurantToItinerary(index, restaurant) {
+  let itineraryDiv = $("#restaurant-itinerary .itinerary-item");
+  itineraryDiv.append("<span class=\"title\" data-id=" + index+ " data-type=\"restaurant\">" + restaurant.name + "</span>");
+  itineraryDiv.append("<button class=\"btn btn-xs btn-danger remove btn-circle\">x</button>");
+  return globalDrawMarker('restaurant', restaurant.place.location);
+}
+
+function addActivityToItinerary(index, activity) {
+  let itineraryDiv =  $("#activity-itinerary .itinerary-item");
+  itineraryDiv.append("<span class=\"title\" data-id=" + index+ " data-type=\"activity\">" + activity.name + "</span>");
+  itineraryDiv.append("<button class=\"btn btn-xs btn-danger remove btn-circle\">x</button>");
+  return globalDrawMarker('activity', activity.place.location);
+}
+
+function clearItinerary() {
+  let hotelDiv = $("#hotel-itinerary .itinerary-item");
+  let restaurantDiv = $("#restaurant-itinerary .itinerary-item");
+  let activityDiv = $("#activity-itinerary .itinerary-item");
+  hotelDiv.empty();
+  restaurantDiv.empty();
+  activityDiv.empty();
+  dayItineraries[currentDay].hotels.forEach(function(hotelWithMarker){
+    hotelWithMarker.marker();
+  })
+   dayItineraries[currentDay].restaurants.forEach(function(restaurantWithMarker){
+    restaurantWithMarker.marker();
+  })
+   dayItineraries[currentDay].activities.forEach(function(activityWithMarker){
+    activityWithMarker.marker();
+  })
 }
 
 $("#options-panel").on('click', 'button', function() {
-  let index = $(this).prev().val();
-  let itineraryDiv, drawMarker;
+  let dbId = $(this).prev().val()-1;
+  let itenaryIndex;
+  let drawMarker;
   if($(this).prev().data("type") === 'hotel') {
-    itineraryDiv = $("#hotel-itinerary .itinerary-item")
-    itineraryDiv.append("<span class=\"title\" data-id=" + index + " data-type=\"hotel\">" + hotels[index].name + "</span>");
-    itineraryDiv.append("<button class=\"btn btn-xs btn-danger remove btn-circle\">x</button>");
-    drawMarker = globalDrawMarker('hotel', hotels[index].place.location);
-    dayItineraries[currentDay].hotels.push({hotel: hotels[index], marker: drawMarker});
+    drawMarker = addHotelToItinerary(dbId, hotels[dbId]);
+    dayItineraries[currentDay].hotels.push({hotel: hotels[dbId], marker: drawMarker});
   } else if($(this).prev().data("type") === 'restaurant') {
-    itineraryDiv = $("#restaurant-itinerary .itinerary-item")
-    itineraryDiv.append("<span class=\"title\" data-id=" + index+ " data-type=\"restaurant\">" + restaurants[index].name + "</span>");
-    itineraryDiv.append("<button class=\"btn btn-xs btn-danger remove btn-circle\">x</button>");
-    drawMarker = globalDrawMarker('restaurant', restaurants[index].place.location);
-    dayItineraries[currentDay].restaurants.push({restaurant: restaurants[index], marker: drawMarker});
+    drawMarker = addRestaurantToItinerary(dbId, restaurants[dbId]);
+    dayItineraries[currentDay].restaurants.push({restaurant: restaurants[dbId], marker: drawMarker});
   } else if($(this).prev().data("type") === 'activity') {
-    itineraryDiv =  $("#activity-itinerary .itinerary-item")
-    itineraryDiv.append("<span class=\"title\" data-id=" + index+ " data-type=\"activity\">" + activities[index].name + "</span>");
-    itineraryDiv.append("<button class=\"btn btn-xs btn-danger remove btn-circle\">x</button>");
-    drawMarker = globalDrawMarker('activity', activities[index].place.location);
-    dayItineraries[currentDay].activities.push({activity: activities[index], marker: drawMarker});
+    drawMarker = addActivityToItinerary(dbId, activities[dbId]);
+    dayItineraries[currentDay].activities.push({activity: activities[dbId], marker: drawMarker});
   }
   console.log(dayItineraries[currentDay]);
 });
 
 $("#itinerary").on('click', 'button', function() {
-  let index = $(this).prev().data('id');
-  let indexToRemove;
+  let index = $(this).prev().data('id')-1;
   if ($(this).prev().data('type') === 'hotel'){
     for(var i = 0; i < dayItineraries[currentDay].hotels.length; i++) {
       if(dayItineraries[currentDay].hotels[i].hotel.id === hotels[index].id) {
@@ -150,6 +183,37 @@ $("#itinerary").on('click', 'button', function() {
 
 $('.day-buttons').on('click', '#day-add', function() {
   var nextDay = +$(this).prev().text() + 1;
-  $(this).before("<button class=\"btn btn-circle day-btn\">" + nextDay + "</button>")
+  $(this).before("<button class=\"btn btn-circle day-btn\" id=\"" + nextDay + "\">" + nextDay + "</button>")
+  dayItineraries.push(new GenerateItinerary());
 })
 
+$('.day-buttons').on('click', 'button', function() {
+  if ($(this).attr('id') === "day-add") return;
+  clearItinerary();
+  currentDay = $(this).attr('id') - 1;
+  $(".day-buttons .current-day").removeClass("current-day");
+  $(this).addClass("current-day");
+  $("#day-title span").text("Day " + (currentDay + 1));
+  let index, drawMarker;
+  dayItineraries[currentDay].hotels.forEach(function(hotelWithMarker) {
+    index = hotelWithMarker.hotel.id;
+    drawMarker = addHotelToItinerary(index, hotelWithMarker.hotel);
+    hotelWithMarker.marker = drawMarker;
+  });
+  dayItineraries[currentDay].restaurants.forEach(function(restaurantWithMarker) {
+    index = restaurantWithMarker.restaurant.id;
+    drawMarker = addRestaurantToItinerary(index, restaurantWithMarker.restaurant);
+    restaurantWithMarker.marker = drawMarker;
+  });
+  dayItineraries[currentDay].activities.forEach(function(activityWithMarker) {
+    index = activityWithMarker.activity.id;
+    drawMarker = addActivityToItinerary(index, activityWithMarker.activity);
+    activityWithMarker.marker = drawMarker;
+  });
+})
+
+$("#day-title").on('click', 'button', function() {
+  clearItinerary();
+  dayItineraries.splice(currentDay, 1);
+  
+})
